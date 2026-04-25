5528ad8dff810e980954514c6cd2daf7153cff92
Nurzhanov Ramazan <you@example.com>
2026-04-25 06:54:42 +0600

    final fix frontend + backend

--- frontendbank/src/utils/routes.ts
+++
@@ -1,8 +0,0 @@
-import type { Role } from '@/types/domain'
-
-export const ROLE_ROUTES: Record<Role, string> = {
-  client: '/client',
-  moderator: '/moderator',
-  admin_it: '/admin/it',
-  admin_bank: '/admin/bank',
-}
